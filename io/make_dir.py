f33cb1cb57dc0b8f756e35631abd16cf27d76e11
杜明昊 <duminghao@Doom.local>
2019-11-16 22:00:12 +0800

    mkdir modified

--- io/make_dir.py
+++ io/make_dir.py
@@ -8,9 +8,13 @@ def mkdir(path: str):
     folder = os.path.exists(path)
     if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
         os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
-        print ("---  new folder...  ---")
-        print ("---  %s Done  ---" %path)
+        print("---  new folder...  ---")
+        print("---  OK  ---")
     else:
-        print ("---  There is this folder!  ---")
-
+        shutil.rmtree(path)
+        print("---  There is this folder!  Delete it ---")
+        os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
+        print("---  new folder...  ---")
+        print("---  OK  ---")
+        
 
